8f0f88b31f10030fc6195200575c6a18934d985f
mqcah <mqcha94@outlook.jp>
2020-11-08 23:33:37 +0900

    pakage

--- lesson5/src/dogData.js
+++ lesson5/src/dogData.js
@@ -1,8 +1,20 @@
-// "./dog"(相対パス)を書き換えてください。
+// readline-syncをインポートしてください
+import readlineSync from "readline-sync";
+
 import Dog from "../class/dog";
 
 const dog1 = new Dog("レオ", 4, "チワワ");
-const dog2 = new Dog("ベン", 2, "プードル");
+
+// readlineSync.questionを使って書き換えてください
+const name = readlineSync.question("名前を入力してください: ");
+
+// readlineSync.questionIntを使って書き換えてください
+const age = readlineSync.questionInt("年齢を入力してください: ");
+
+// readlineSync.questionを使って書き換えてください
+const breed = readlineSync.question("犬種を入力してください: ");
+
+const dog2 = new Dog(name, age, breed);
 
 export {
     dog1,
