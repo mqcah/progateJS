69380a4ebb50eb9ffe9906f80b1d3b5dd76fab86
mqcah <mqcha94@outlook.jp>
2020-11-18 22:51:14 +0900

    lesson4

---
+++ jquery/sccript4.js
@@ -0,0 +1,10 @@
+$(function() {
+        // showメソッドを用いて、「#title」要素を表示してください
+        $('#title').show();
+
+        // fadeInメソッドを用いて、「#image」要素を表示してください
+        $('#image').fadeIn();
+
+    }
+
+);
